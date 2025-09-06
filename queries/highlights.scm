2d9cd56e2bf90f6ad849554538c584144e25e58a
Perry <aidanpj18@gmail.com>
2025-09-05 23:01:20 -0400

    update highlights Comment out problematic highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,19 +1,19 @@
 ; Comments
 (comment) @comment
-(multiline_comment) @comment
+; (multiline_comment) @comment
 
 ; Literals
 (string_literal) @string
 (number_literal) @number
 (boolean_literal) @boolean
-(char_literal) @character
+; (char_literal) @character
 (unit_literal) @constant
 
 ; Types
 (basic_type) @type
 (function_type) @type
-(array_type) @type
-(tuple_type) @type
+; (array_type) @type
+; (tuple_type) @type
 
 ; Identifiers in specific contexts
 (function_declaration
@@ -30,52 +30,53 @@
   (identifier) @variable)
 (parameter
   (identifier) @parameter)
-(module_declaration
-  (identifier) @namespace)
+; (module_declaration
+;   (identifier) @namespace)
 
 ; Fields
 (struct_field
   (identifier) @property)
-(struct_field_initializer
-  (identifier) @property)
-(member_expression
-  (identifier) @property)
+; (struct_field_initializer
+;   (identifier) @property)
+; (member_expression
+;   (identifier) @property)
 
 ; Statement nodes that represent keywords
-(break_statement) @keyword
-(continue_statement) @keyword
-(return_statement) @keyword
+; (break_statement) @keyword
+; (continue_statement) @keyword
+; (return_statement) @keyword
 ; Macro-related keywords
 ; To change the behavior of these keywords, change '@keyword.macro' to '@function.macro' or similar
-(macro_declaration) @keyword.macro
-(macro_expression) @keyword.macro
-(macro_invocation) @keyword.macro
+
+; (macro_declaration) @keyword.macro
+; (macro_expression) @keyword.macro
+; (macro_invocation) @keyword.macro
 
 ; Safe keywords (excluding the problematic ones)
 "fn" @keyword
 "proc" @keyword
 "let" @keyword
-"var" @keyword
-"const" @keyword
-"mut" @keyword
+; "var" @keyword
+; "const" @keyword
+; "mut" @keyword
 "struct" @keyword
 "kind" @keyword
-"impl" @keyword
+; "impl" @keyword
 "end" @keyword
 "if" @keyword
 "else" @keyword
 "then" @keyword
-"while" @keyword
-"for" @keyword
-"in" @keyword
+; "while" @keyword
+; "for" @keyword
+; "in" @keyword
 "do" @keyword
-"mod" @keyword
-"import" @keyword
+; "mod" @keyword
+; "import" @keyword
 "pub" @keyword
-"as" @keyword
+; "as" @keyword
 "enum" @keyword
-"match" @keyword
-"where" @keyword
+; "match" @keyword
+; "where" @keyword
 "return" @keyword
 
 ; Pattern matching for problematic keywords in identifiers
@@ -98,23 +99,23 @@
 "=>" @operator
 "^" @operator
 "%" @operator
-"+=" @operator
-"-=" @operator
-"*=" @operator
-"/=" @operator
-"|>" @operator
-"<-" @operator
-"@" @operator
-"~" @operator
+; "+=" @operator
+; "-=" @operator
+; "*=" @operator
+; "/=" @operator
+; "|>" @operator
+; "<-" @operator
+; "@" @operator
+; "~" @operator
 
 ; Punctuation
 "(" @punctuation.delimiter
 ")" @punctuation.delimiter
-"{" @punctuation.delimiter
-"}" @punctuation.delimiter
-"[" @punctuation.delimiter
-"]" @punctuation.delimiter
+; "{" @punctuation.delimiter
+; "}" @punctuation.delimiter
+; "[" @punctuation.delimiter
+; "]" @punctuation.delimiter
 "," @punctuation.delimiter
 "." @punctuation.delimiter
 ":" @punctuation.delimiter
-";" @punctuation.delimiter
+; ";" @punctuation.delimiter
